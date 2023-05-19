9a645b55ad6fe3a6f143feedbbb9146172ff14ea
Oliver Jay <123472199+LoreviQ@users.noreply.github.com>
2023-05-19 16:15:10 +0100

    Add files via upload

---
+++ OneDHTSensor.ino
@@ -0,0 +1,29 @@
+#include <DHT.h>;
+
+#define DHTPIN1 2
+
+DHT dht = (DHTPIN1, DHT22);
+
+float humidity;
+float temperature;
+
+void setup()
+{
+    Serial.begin(9600);
+    dht.begin();
+}
+
+void loop()
+{
+    temperature = dht.readTemperature();
+    humidity = dht.readHumidity();
+
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print("%");
+    Serial.print("  |  ");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println("°C ~ ");
+    delay(5000);
+}
