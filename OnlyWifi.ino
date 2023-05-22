59549cb4da43a68e135c508ca9da2f5b86605152
Oliver Jay <123472199+LoreviQ@users.noreply.github.com>
2023-05-22 16:48:56 +0100

    Update OnlyWifi.ino

--- OnlyWifi.ino
+++ OnlyWifi.ino
@@ -19,7 +19,7 @@ struct tm *ptm = gmtime ((time_t *)&epochTime);
 ESP8266WebServer server(80);
 
 // Initialising variables
-String SendHTML(float TemperatureWeb,float HumidityWeb, String TimeWeb, String DateWeb);
+String SendHTML(String TimeWeb, String DateWeb);
 void handle_OnConnect();
 void handle_NotFound();
 String formattedTime;
@@ -29,8 +29,7 @@ int Month;
 int Year;
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(DHTPin, INPUT);           
+  Serial.begin(115200);         
 
   Serial.println("Connecting to ");
   Serial.println(ssid);
@@ -48,7 +47,6 @@ void setup() {
   server.on("/", handle_OnConnect);
   server.onNotFound(handle_NotFound);
   server.begin();
-  dht.begin();
   timeClient.begin();
 }
 void loop() {
@@ -72,16 +70,14 @@ void handle_OnConnect() {
  
   formattedTime = timeClient.getFormattedTime(); 
   Date = String(currentYear) + "-" + String(currentMonth) + "-" + String(monthDay);
-  Temperature = dht.readTemperature(); 
-  Humidity = dht.readHumidity(); 
-  server.send(200, "text/html", SendHTML(Temperature,Humidity,formattedTime,Date)); 
+  server.send(200, "text/html", SendHTML(formattedTime,Date)); 
 }
 
 void handle_NotFound(){
   server.send(404, "text/plain", "Not found");
 }
 
-String SendHTML(float TemperatureWeb,float HumidityWeb, String TimeWeb,String DateWeb){
+String SendHTML(String TimeWeb,String DateWeb){
   String ptr = "<!DOCTYPE html> <html>\n";
   ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
   ptr +="<title>ESP8266 Global Server</title>\n";
@@ -97,12 +93,6 @@ String SendHTML(float TemperatureWeb,float HumidityWeb, String TimeWeb,String Da
   ptr +="<p>Time: ";
   ptr +=(String)TimeWeb;
   ptr +="</p>";
-  ptr +="<p>Temperature: ";
-  ptr +=(int)TemperatureWeb;
-  ptr +="C</p>";
-  ptr +="<p>Humidity: ";
-  ptr +=(int)HumidityWeb;
-  ptr +="%</p>";
   
   ptr +="</div>\n";
   ptr +="</body>\n";
