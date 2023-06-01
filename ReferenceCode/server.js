53700c2ce465764df1db6185cf47704fae4ffe21
LoreviQ <lorevi@protonmail.com>
2023-06-01 17:43:19 +0100

    Added user/pswd

--- ReferenceCode/server.js
+++ ReferenceCode/server.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // fetches root files from client/build
 //app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 // May only be exist once in app
-mongoose.connect("mongodb://my_user:my_pwd@localhost:27017/mern", { useNewUrlParser: true });
+mongoose.connect("mongodb://testUser:testPassword@localhost:27017/test", { useNewUrlParser: true });
 
 const Schema = mongoose.Schema;
 const memberSchema = new Schema({
     firstName: String,
-    lastName: String
+    lastName: String,
 });
 const Member = mongoose.model("member", memberSchema);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.get('/favicon.ico', (req, res) => {
+app.get("/favicon.ico", (req, res) => {
     res.sendFile(path.join(`${__dirname}/favicon.ico`));
 });
 
-app.get('/members', (req, res) => {
-    Member.find({}, "firstName lastName").then(members => {
+app.get("/members", (req, res) => {
+    Member.find({}, "firstName lastName").then((members) => {
         if (members !== null && members.length > 0) {
             res.write(JSON.stringify(members));
         } else {
