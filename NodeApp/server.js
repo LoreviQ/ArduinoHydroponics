affde827e97662c233a25f43fb474102943f8e3c
LoreviQ <lorevi@protonmail.com>
2023-06-05 14:47:48 +0100

    Changed endpoint to only add arduino

--- NodeApp/server.js
+++ NodeApp/server.js
@@ -14,33 +14,33 @@ app.get("/", (req, res) => {
 
 // Setup endpoint that arduino calls on boot.
 // Returns array of ids corresponding to the Arduino and Sensor
-app.post("/setup", async (req, res) => {
+app.post("/setup/arduino", async (req, res) => {
     try {
         // Arduino
-        var arduinoJSON = req.body.arduino;
-        var arduino = await Arduino.find({'name' : arduinoJSON.name}).exec();
+        var arduino = await Arduino.find({'name' : req.body.name}).exec();
         arduino = arduino[0];
         if (!arduino) {
-            var arduino = await Arduino.create(arduinoJSON);
+            var arduino = await Arduino.create(req.body);
         }
-        var ids = [arduino._id];
-
-        // Sensors
-        for (const sensorJSON of req.body.sensors) {
-            sensorJSON.arduinoID = arduino._id;
-            var sensor = await Sensor.find({'name' : sensorJSON.name, 'arduinoID' : sensorJSON.arduinoID}).exec();
-            sensor = sensor[0];
-            if (!sensor) {
-                sensor = await Sensor.create(sensorJSON);
-            }
-            ids.push(sensor._id);
-        }
-        res.status(200).json(ids);
+        res.status(200).json(arduino._id);
     } catch (error) {
         console.log(error.message);
         res.status(500).json({ message: error.message });
     }
 });
+
+/*
+// Sensors
+for (const sensorJSON of req.body.sensors) {
+    sensorJSON.arduinoID = arduino._id;
+    var sensor = await Sensor.find({'name' : sensorJSON.name, 'arduinoID' : sensorJSON.arduinoID}).exec();
+    sensor = sensor[0];
+    if (!sensor) {
+        sensor = await Sensor.create(sensorJSON);
+    }
+    ids.push(sensor._id);
+}
+*/
 
 mongoose.set("strictQuery", true);
 mongoose
