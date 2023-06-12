4281a7ed15fb4d5ee0bc9f22fc9e9f45077fb2a0
LoreviQ <lorevi@protonmail.com>
2023-06-12 16:03:15 +0100

    Added path

--- NodeApp/server.js
+++ NodeApp/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const port = 8000;
 
